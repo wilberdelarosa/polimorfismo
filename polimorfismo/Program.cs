67fe1d6f367a7214794e65a8138a2416e0517996
WILBER ALFREDO DE LA ROSA MARTE <wd2022-0547@uce.edu.do>
2023-10-07 12:49:47 -0400

    Agregar archivos de proyecto.

---
+++ polimorfismo/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+
+class Persona
+
+{
+
+    public string Nombre;
+    public int Edad;
+    public string NIF;
+
+
+    public virtual void Cumpleanos()
+    {
+       
+        Console.WriteLine("incrementa edad de persona");
+    }
+    public Persona(string nombre, int edad, string nif)
+    {
+        Nombre = nombre;
+        Edad = edad;
+        NIF = nif;
+
+    }
+
+}
+
+class Trabajador: Persona
+
+{
+    int Sueldo;
+
+    Trabajador(string nombre, int edad, string nif, int sueldo) : base(nombre, edad, nif)
+    { 
+        
+        Sueldo = sueldo; 
+    
+    }
+    // polimorfismo
+    public static void Main()
+    {
+
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Clear();
+
+        Persona p = new Trabajador("Josan", 22, "77588260-Z" , 100000);
+
+        p.Cumpleanos();
+        
+
+    }
+
+}
+
